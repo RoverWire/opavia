97b992571423487cae2a04a19a6b44cb487ebf91
Luis Felipe <slashcat@gmail.com>
2012-08-23 00:00:21 -0500

    Terminar MY_Model

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -7,11 +7,32 @@ class MY_Model extends CI_Model {
 	protected $_archivos 	= array();
 	protected $_id 			= 'id';
 
+	protected $pre_agregar		= array();
+	protected $pos_agregar		= array();
+	protected $pre_actualizar 	= array();
+	protected $pos_actualizar	= array();
+	protected $pre_eliminar		= array();
+	protected $pos_eliminar		= array();
+
+	protected $callback_parametros	= array();
+
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->helper('inflector');
+		$this->definir_base();
 		$this->definir_tabla();
+	}
+
+	/* --------------------------------------------------------------
+     * FUNCIONES DE USO INTERNO
+     * ------------------------------------------------------------ */
+
+	protected function definir_base()
+	{
+		if (!empty($this->_db)) {
+			$this->db = $this->load->database($this->_db, TRUE);
+		}
 	}
 
 	protected function definir_tabla()
@@ -21,11 +42,55 @@ class MY_Model extends CI_Model {
 		}
 	}
 
+	protected function trigger($event, $datos = FALSE)
+	{
+		if (isset($this->$event) && is_array($this->$event)) {
+			foreach ($this->$event as $method) {
+				if (strpos($method, '(')) {
+					preg_match('/([a-zA-Z0-9\_\-]+)(\(([a-zA-Z0-9\_\-\., ]+)\))?/', $method, $matches);
+					$method = $matches[1];
+					$this->callback_parametros = explode(',', $matches[3]);
+				}
+
+				$datos = call_user_func_array(array($this, $method), array($datos));
+			}
+		}
+
+		return $datos;
+	}
+
+	public function serialize($datos)
+	{
+		foreach ($this->callback_parametros as $campo) {
+			$datos[$campo] = serialize($datos[$campo]);
+		}
+	}
+
+	public function unserialize($datos)
+	{
+		foreach ($this->callback_parametros as $campo) {
+            if (is_array($datos)) {
+                $datos[$campo] = unserialize($datos[$campo]);
+            } else {
+                $datos->$campo = unserialize($datos->$campo);
+            }
+        }
+
+        return $datos;
+	}
+
+	/* --------------------------------------------------------------
+     * FUNCIONES GENÉRICAS
+     * ------------------------------------------------------------ */
+
 	public function agregar($datos)
 	{
 		if (is_array($datos)) {
+			$datos = $this->trigger('pre_agregar', $datos);
 			$this->db->insert($this->_tabla, $datos);
-			return $this->db->insert_id();
+			$id = $this->db->insert_id();
+			$this->trigger('pos_agregar', $id);
+			return $id;
 		} else {
 			return FALSE;
 		}
@@ -43,8 +108,10 @@ class MY_Model extends CI_Model {
 	public function actualizar($id, $datos)
 	{
 		if (is_array($datos) && !empty($id)) {
+			$datos = $this->trigger('pre_actualizar', $datos);
 			$this->db->where($this->_id, $id)->update($this->_tabla, $datos);
-			return $this->db->affected_rows();
+			$rows = $this->db->affected_rows();
+			$this->trigger('pos_actualizar', array($datos, $rows));
 		} else {
 			return FALSE;
 		}
@@ -52,20 +119,85 @@ class MY_Model extends CI_Model {
 
 	public function eliminar($id)
 	{
+		if (empty($id) || (is_array($id) && count($id) < 1)) {
+			return FALSE;
+		}
+
 		if (!is_array($id)) {
 			$id = array($id);
 		}
 
 		if (count($this->_archivos) > 0) {
 			$campos = implode(', ', $this->_archivos);
-			$query = $this->db->select($campos, FALSE)->where_in($this->_id, $id)->get($this->_tabla);
+			$query = $this->db->select($campos)->where_in($this->_id, $id)->get($this->_tabla);
 
 			foreach ($query->result() as $row) {
 				foreach ($this->_archivos as $archivo) {
-					
+					if (isset($row->$archivo) && !empty($row->$archivo)) {
+						if (file_exists('.'.$row->$archivo)) {
+							unlink('.'.$row->$archivo);
+						}
+					}
 				}
 			}
 		}
+
+		$this->db->where_in($this->_id, $id)->delete($this->_tabla);
+
+		return $this->db->affected_rows();
+	}
+
+	public function listar()
+	{
+		return $this->db->get($this->_tabla);
+	}
+
+	/* --------------------------------------------------------------
+     * QUERY HELPERS ::EXPERIMENTAL::
+     * ------------------------------------------------------------ */
+
+	public function get()
+	{
+		return $this->db->get($this->_tabla);
+	}
+
+	public function last_query()
+	{
+		return $this->db->last_query();
+	}
+
+	public function count()
+	{
+		return $this->db->count_all_results($this->_tabla);
+	}
+
+	public function count_all()
+	{
+		return $this->db->count_all($this->_tabla);
+	}
+	
+	public function __call($metodo, $parametros)
+	{
+		if (method_exists($this->db, $metodo)) {
+			call_user_func_array(array($this->db, $metodo), $parametros);
+			return $this;
+		} else {
+			return FALSE;
+		}		
+	}
+
+	/* --------------------------------------------------------------
+     * UTILERIAS
+     * ------------------------------------------------------------ */
+
+	public static function calcular_id()
+	{
+		return date("Ymds"). sprintf("%02d", rand(0,99));
+	}
+
+	public function tabla()
+	{
+		return $this->_tabla;
 	}
 
 }
---
+++ application/modules/usuarios/models/usuario.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usuario extends MY_Model {
+
+	public function __construct()
+	{
+		parent::__construct();		
+	}
+
+}
+
+/* End of file usuario.php */
+/* Location: ./application/models/usuario.php */
