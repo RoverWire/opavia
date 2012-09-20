037112ba2dd67dd0cc400d87ab38591411bfa761
Luis Felipe Perez <slashcat@gmail.com>
2012-09-20 15:40:42 -0500

    auth improve

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,9 +2,25 @@
 
 class MY_Controller extends CI_Controller {
 
+	private   $paginas_publicas = array('/', 'index.php/', 'usuarios/login', 'usuarios/logout');
+	protected $permitidos = array(0);
+
 	public function __construct()
 	{
 		parent::__construct();
+
+		/* Verificamos si está permitido el acceso */
+		$actual = $this->uri->segment(1, '') . '/' . $this->uri->segment(2, '');
+		$publica = in_array($actual, $this->paginas_publicas);
+		if( ! $publica AND ! $this->session->userdata('id') )
+		{
+			redirect('usuarios/login');
+		}
+
+		/* Se verifica que el usuario tenga privilegios de acceso */
+		if (in_array($this->session->userdata('tipo'), $this->permitidos)) {
+			redirect('usuarios/login');
+		}
 	}
 
 }
